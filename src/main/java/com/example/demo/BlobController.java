ab2129a3919e842c07fae342d1a42afb292d4cd9
Chi Luong <chi.luong@gm.com>
2022-12-08 21:29:11 -0500

    add managed identity

--- src/main/java/com/example/demo/BlobController.java
+++ src/main/java/com/example/demo/BlobController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.identity.ManagedIdentityCredential;
 import com.azure.identity.ManagedIdentityCredentialBuilder;
+import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -25,8 +26,8 @@ import java.util.stream.Collectors;
 @RequestMapping("blob")
 public class BlobController {
 
-    @Value("azure-blob://sample-webapp/delegate.json")
-    private Resource blobFile;
+//    @Value("azure-blob://sample-webapp/delegate.json")
+//    private Resource blobFile;
 //    String fileName = "delegate.json";
 //    String container = "sample-webapp";
 
@@ -45,47 +46,53 @@ public class BlobController {
 //                .endpoint("https://secondtried.blob.core.windows.net/")
 //                .credential(managedIdentityCredential)
 //                .buildClient();
+        BlobServiceClient blobStorageClient = new BlobServiceClientBuilder()
+                .endpoint("https://secondtried.blob.core.windows.net")
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+        BlobClient blobClient = blobStorageClient.getBlobContainerClient("sample-webapp").getBlobClient("delegate.json");
+        Resource blobFile = ((Resource) blobClient);
 
         return StreamUtils.copyToString(
-                this.blobFile.getInputStream(),
+                blobFile.getInputStream(),
                 Charset.forName("UTF-8"));
     }
 
-    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String addDelegate(@RequestBody Delegate delegate) throws IOException {
-        try (OutputStream os = ((WritableResource) this.blobFile).getOutputStream()) {
-            String result = StreamUtils.copyToString(this.blobFile.getInputStream(), Charset.forName("UTF-8"));
-            ObjectMapper mapper = new ObjectMapper();
-//            if (!result.isEmpty()){
-            List<Delegate> delegateList = new ArrayList<>();
-            delegateList = mapper.readValue(result, new TypeReference<List<Delegate>>() {});
-            List<Integer> allIds = delegateList.stream().map(item -> item.getId()).collect(Collectors.toList());
-            delegate.setId(allIds.get(allIds.size()-1)+1);
-            delegateList.add(delegate);
-            String jsonString = mapper.writeValueAsString(delegateList);
-            os.write(jsonString.getBytes());}
+//    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+//    public String addDelegate(@RequestBody Delegate delegate) throws IOException {
+//        try (OutputStream os = ((WritableResource) this.blobFile).getOutputStream()) {
+//            String result = StreamUtils.copyToString(this.blobFile.getInputStream(), Charset.forName("UTF-8"));
+//            ObjectMapper mapper = new ObjectMapper();
+////            if (!result.isEmpty()){
+//            List<Delegate> delegateList = new ArrayList<>();
+//            delegateList = mapper.readValue(result, new TypeReference<List<Delegate>>() {});
+//            List<Integer> allIds = delegateList.stream().map(item -> item.getId()).collect(Collectors.toList());
+//            delegate.setId(allIds.get(allIds.size()-1)+1);
+//            delegateList.add(delegate);
+//            String jsonString = mapper.writeValueAsString(delegateList);
+//            os.write(jsonString.getBytes());}
+////        }
+//        return "file was updated";
+//    }
+//
+//    @DeleteMapping("/delete/{id}")
+//    public String deleteDelegate(@PathVariable("id") Integer id) throws IOException{
+//        try (OutputStream os = ((WritableResource) this.blobFile).getOutputStream()) {
+//            String result = StreamUtils.copyToString(this.blobFile.getInputStream(), Charset.forName("UTF-8"));
+//            ObjectMapper mapper = new ObjectMapper();
+//            List<Delegate> delegateList = new ArrayList<>();
+//            delegateList = mapper.readValue(result, new TypeReference<List<Delegate>>() {});
+//            List<Integer> allIds = delegateList.stream().map(delegate -> delegate.getId()).collect(Collectors.toList());
+//            if (allIds.contains(id)) {
+//                delegateList.removeIf(delegate -> delegate.getId().equals(id));
+//                String jsonString = mapper.writeValueAsString(delegateList);
+//                os.write(jsonString.getBytes());
+//                return "delete successfully";
+//            } else {
+//                String jsonString = mapper.writeValueAsString(delegateList);
+//                os.write(jsonString.getBytes());
+//                return "this id does not exist";
+//            }
 //        }
-        return "file was updated";
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public String deleteDelegate(@PathVariable("id") Integer id) throws IOException{
-        try (OutputStream os = ((WritableResource) this.blobFile).getOutputStream()) {
-            String result = StreamUtils.copyToString(this.blobFile.getInputStream(), Charset.forName("UTF-8"));
-            ObjectMapper mapper = new ObjectMapper();
-            List<Delegate> delegateList = new ArrayList<>();
-            delegateList = mapper.readValue(result, new TypeReference<List<Delegate>>() {});
-            List<Integer> allIds = delegateList.stream().map(delegate -> delegate.getId()).collect(Collectors.toList());
-            if (allIds.contains(id)) {
-                delegateList.removeIf(delegate -> delegate.getId().equals(id));
-                String jsonString = mapper.writeValueAsString(delegateList);
-                os.write(jsonString.getBytes());
-                return "delete successfully";
-            } else {
-                String jsonString = mapper.writeValueAsString(delegateList);
-                os.write(jsonString.getBytes());
-                return "this id does not exist";
-            }
-        }
-    }
+//    }
 }
